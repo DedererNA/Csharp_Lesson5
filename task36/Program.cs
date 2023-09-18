6a5211d9978ae30be20c5bf44ee13b5c34dfe537
Nikolay Dederer <cmrddrr@gmail.com>
2023-09-18 13:40:32 +0300

    task36 finished

---
+++ task36/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] RandArray (int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i=0; i<size; i++)
+    {
+        array[i]=new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+int OddSum (int[] array)
+{
+    int sum=0;
+    for (int i=1; i<array.Length; i+=2)
+    {
+        sum+=array[i];
+    }
+    return sum;
+}
+
+int[] newarray = RandArray (8, -20, 20);
+Console.WriteLine(string.Join(" ", newarray));
+
+Console.WriteLine($"{(OddSum(newarray))}");
