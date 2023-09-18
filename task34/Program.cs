77f44d7a7a60778b512fbfe64fce00e0c1b53ab9
Nikolay Dederer <cmrddrr@gmail.com>
2023-09-18 13:34:33 +0300

    task34 finished

---
+++ task34/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] RandArray (int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i=0; i<size; i++)
+    {
+        array[i]=new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+int EvenCount (int[] array)
+{
+    int count=0;
+    for (int i=0; i<array.Length; i++)
+    {
+        if (array[i]%2==0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] newarray = RandArray (5, 100, 999);
+Console.WriteLine(string.Join(" ", newarray));
+
+Console.WriteLine($"{(EvenCount(newarray))}");
